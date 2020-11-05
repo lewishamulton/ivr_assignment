794033b5aa5c0087a7d3ad8fef864e1879d846b0
lewishamulton <47535585+lewishamulton@users.noreply.github.com>
2020-11-05 19:56:55 +0000

    Sinusoidal Trajectory
    -Added the sinusoidal trajectory for joints 2,3,4 in image1.py

--- ivr_assignment/src/image1.py
+++ ivr_assignment/src/image1.py
@@ -21,8 +21,26 @@ class image_converter:
     self.image_pub1 = rospy.Publisher("image_topic1",Image, queue_size = 1)
     # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
     self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw",Image,self.callback1)
+
+    #set up publisher to send joint angles to the robot
+    self.robot_joint1_pub = rospy.Publisher("/robot/joint1_position_controller/command",Float64,queue_size=10)
+    self.robot_joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command",Float64,queue_size=10)
+    self.robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command",Float64,queue_size=10)
+    self.robot_joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command",Float64,queue_size=10)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
+
+    #start time for robot
+    self.time_trajectory = rospy.get_time()
+
+
+  def defineSinusoidalTrajectory(self):
+    #get current time
+    cur_time = np.array([rospy.get_time() - self.time_trajectory])
+    joint2_trajectory = float(np.pi/2*np.sin(np.pi/15*cur_time))
+    joint3_trajectory = float(np.pi/2*np.sin(np.pi/18*cur_time))
+    joint4_trajectory = float(np.pi/2*np.sin(np.pi/20*cur_time))
+    return np.array([joint2_trajectory,joint3_trajectory,joint4_trajectory])
 
 
   # Recieve data from camera 1, process it, and publish
@@ -32,15 +50,24 @@ class image_converter:
       self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
-    
+
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
 
     im1=cv2.imshow('window1', self.cv_image1)
     cv2.waitKey(1)
+
+    #Get joint angles
+    j_angle = self.defineSinusoidalTrajectory()
+
     # Publish the results
-    try: 
+    try:
       self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
+
+      self.robot_joint2_pub.publish(j_angle[0])
+      self.robot_joint3_pub.publish(j_angle[1])
+      self.robot_joint4_pub.publish(j_angle[2])
+
     except CvBridgeError as e:
       print(e)
 
@@ -56,5 +83,3 @@ def main(args):
 # run the code if the node is called
 if __name__ == '__main__':
     main(sys.argv)
-
-
